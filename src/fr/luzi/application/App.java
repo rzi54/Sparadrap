8b2c5ca70bfa8e4176d866bc54f04876d493cb61
rzi54 <randy.luzi@gmail.com>
2023-09-06 10:08:56 +0200

    ECF - Gestionnaire Sparadrap

--- src/fr/luzi/application/App.java
+++ src/fr/luzi/application/App.java
@@ -5,7 +5,7 @@ package fr.luzi.application;
  * Prototype d'application développée dans le cadre du premier examen (ECF).
  * Type : Gestionnaire.
  * Déroulement de l'examen : du 29/08/2023 au 06/09/2023.
- * Formation concourue : Concepteur Développeur d'Applications.
+ * Titre visé : Concepteur Développeur d'Applications.
  * Formateur CDA : Jérôme BOEBION
  * Centre de formation : AFPA - Centre de Pompey, 56 Sq. Eugène Herzog, 54390 Frouard
  * 
@@ -1280,7 +1280,7 @@ public class App extends JFrame {
 	};
 
 	/*
-	 * ---------------------> Méthodes spécifiques
+		 * ---------------------> Méthodes spécifiques
 	 */
 
 	/*
